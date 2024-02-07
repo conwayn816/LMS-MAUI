b7be5ffffb785976853a9bd54bcbe62873acf09c
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-07 14:26:23 -0500

    git ignore

--- MAUI-App/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MAUI-App.AssemblyInfo.cs
+++ MAUI-App/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MAUI-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUI-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c260892cb1edbb8a3192f90b2ef59dcbe851a958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+245b833b9528d422e2ebfda105bcaf8f7d5a3fbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUI-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUI-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
