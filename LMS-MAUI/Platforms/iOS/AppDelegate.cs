ea7ac2f3acf315c1bb6582fbbada0d304d06949b
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-07 14:07:21 -0500

    init

---
+++ LMS-MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LMS_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
