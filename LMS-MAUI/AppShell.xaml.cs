c260892cb1edbb8a3192f90b2ef59dcbe851a958
Nathan Conway <conwayn816@gmail.com>
2024-02-07 14:11:30 -0500

    Delete LMS-MAUI directory

--- LMS-MAUI/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LMS_MAUI;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- LMS-MAUI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace LMS_MAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
