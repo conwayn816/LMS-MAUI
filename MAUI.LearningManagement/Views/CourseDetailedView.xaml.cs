63d121b83ed862346c8691404c53cb767bf3601f
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-04-03 17:16:51 -0400

    added roster, module, and assignment placeholder

--- MAUI.LearningManagement/Views/CourseDetailedView.xaml.cs
+++ MAUI.LearningManagement/Views/CourseDetailedView.xaml.cs
@@ -11,6 +11,31 @@ public partial class CourseDetailedView : ContentPage
         BindingContext = new CourseDetailedViewViewModel(courseSelected);
     }
 
+    private void EnrollClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void UnenrollClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CreateAssignmentClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CreateModuleClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CreateContentItemClicked(object sender, EventArgs e)
+    {
+
+    }
+
     private void GoBackClicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync("//CourseManagement");
